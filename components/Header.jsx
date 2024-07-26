33be7e5a302e1e7222b1eb03f2de712456d63824
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-07-26 09:27:26 +0700

    button dark light

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ThemeToogle from './ThemeToogle'
+
+const Header = () => {
+  return (
+    <header>
+        <ThemeToogle />
+    </header>
+  )
+}
+
+export default Header
