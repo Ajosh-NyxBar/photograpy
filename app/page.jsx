c9c0452eedcf4b1478a1dc9372eac32ae092b59c
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-07-29 18:55:14 +0700

    add Review Section

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Features from "@/components/Features";
 import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
+import Reviews from "@/components/Reviews";
 import Services from "@/components/Services";
 import Image from "next/image";
 
@@ -11,6 +12,7 @@ export default function Home() {
       <Features />
       <Services />
       <Gallery />
+      <Reviews />
     </main>
   );
 }
---
+++ components/Reviews.jsx
@@ -0,0 +1,117 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+
+const dataRev = [
+  {
+    ava: "/reviews/avatar-1.png",
+    name: "Jeni",
+    job: "Chief",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique suscipit deserunt voluptates, illo tempore rerum quidem nihil impedit nesciunt vero dolores sunt minima! Provident, corrupti molestiae velit praesentium explicabo numquam.",
+  },
+  {
+    ava: "/reviews/avatar-2.png",
+    name: "Jeni",
+    job: "Chief",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique suscipit deserunt voluptates, illo tempore rerum quidem nihil impedit nesciunt vero dolores sunt minima! Provident, corrupti molestiae velit praesentium explicabo numquam.",
+  },
+  {
+    ava: "/reviews/avatar-3.png",
+    name: "Jeni",
+    job: "Chief",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique suscipit deserunt voluptates, illo tempore rerum quidem nihil impedit nesciunt vero dolores sunt minima! Provident, corrupti molestiae velit praesentium explicabo numquam.",
+  },
+  {
+    ava: "/reviews/avatar-4.png",
+    name: "Jeni",
+    job: "Chief",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique suscipit deserunt voluptates, illo tempore rerum quidem nihil impedit nesciunt vero dolores sunt minima! Provident, corrupti molestiae velit praesentium explicabo numquam.",
+  },
+  {
+    ava: "/reviews/avatar-5.png",
+    name: "Jeni",
+    job: "Chief",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique suscipit deserunt voluptates, illo tempore rerum quidem nihil impedit nesciunt vero dolores sunt minima! Provident, corrupti molestiae velit praesentium explicabo numquam.",
+  },
+  {
+    ava: "/reviews/avatar-6.png",
+    name: "Jeni",
+    job: "Chief",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique suscipit deserunt voluptates, illo tempore rerum quidem nihil impedit nesciunt vero dolores sunt minima! Provident, corrupti molestiae velit praesentium explicabo numquam.",
+  },
+];
+
+const Reviews = () => {
+  return (
+    <section className="mb-12 xl:mb-32">
+      <div className="container mx-auto">
+        <p className="section-title mb-12 text-center mx-auto text-lg">
+          They Say About Our Work
+        </p>
+
+        {/* SWIPER */}
+        <Swiper
+          modules={[Pagination]}
+          slidesPerView={1}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            1400: {
+              slidesPerView: 3,
+            },
+          }}
+          spaceBetween={30}
+          pagination={{
+            clickable: true
+          }}
+          className="h-[450px]"
+        >
+          {dataRev.map((item, index) => {
+            return (
+              <SwiperSlide key={index}>
+               <Card className="min-h-[300px] border border-primary rounded-xl dark:border-gray-200">
+                <CardHeader className="">
+                    <div className="flex flex-col items-start g-x-4">
+                        {/* AVATAR */}
+                        <Image 
+                        src={item.ava}
+                        alt={item.name}
+                        width={70}
+                        height={70}
+                        priority
+                        className="rounded-full"
+                        />
+                        {/* NAME */}
+                        <div className="mt-4">
+                            <CardTitle className="text-lg">{item.name}</CardTitle>
+                            <CardDescription className="text-sm text-gray-500">{item.job}</CardDescription>
+                        </div>
+                    </div>
+                </CardHeader>
+                <CardContent>
+                    <CardDescription className=" text-muted-foreground">{item.review}</CardDescription>
+                </CardContent>
+               </Card>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Reviews;
